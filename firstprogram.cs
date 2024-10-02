0403a0055b5fad9a69ccb92b68bebb5fbe26711f
sakthiviswanathan <sakthims2005@gmail.com>
2024-10-02 15:51:03 -0400

    Update firstprogram.cs

--- firstprogram.cs
+++ firstprogram.cs
@@ -1,10 +1,7 @@
 using System;
 class firstprogram
 {
-    enum Color {Yellow = 1, Blue, Green};
-    static DateTime thisDate = DateTime.Now;
-
-    public static void Main()
+        public static void Main()
     {
         Console.Clear();
         Console.WriteLine("Hello, this my program to test Github Action");   
